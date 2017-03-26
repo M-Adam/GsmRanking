519511c45b9030e0f1ffbaaa1d86db34efb1782d
Adam Mołczan <adam.molczan@outlook.com>
2017-03-26 18:55:24 +0200

    Delete Bower references

--- GsmRanking/Startup.cs
+++ GsmRanking/Startup.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -12,6 +14,8 @@ namespace GsmRanking
 {
     public class Startup
     {
+        public const string AuthenticationScheme = "GsmRankingAuthentication";
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -19,6 +23,13 @@ namespace GsmRanking
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+
             Configuration = builder.Build();
         }
 
@@ -27,8 +38,14 @@ namespace GsmRanking
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry(Configuration);
             // Add framework services.
+
+            //Add EF DbContext
             services.AddMvc();
+
+            services.AddAuthorization();
+            services.AddSingleton<ITempDataProvider, CookieTempDataProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +65,17 @@ namespace GsmRanking
             }
 
             app.UseStaticFiles();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                AuthenticationScheme = AuthenticationScheme,
+                LoginPath = new PathString("/Account/Unauthorized/"),
+                AccessDeniedPath = new PathString("/Account/Forbidden/"),
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                ReturnUrlParameter = "returnUrlParameter",
+                CookieSecure = CookieSecurePolicy.SameAsRequest
+            });
 
             app.UseMvc(routes =>
             {
