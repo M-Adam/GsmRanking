d75c8524609781ba5ab0aa9cde6c920c5c42cfd6
Adam Mołczan <adam.molczan@outlook.com>
2017-04-09 15:17:27 +0200

    Password hasher and ValidationController

---
+++ GsmRanking/Common/Hasher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GsmRanking.Common
+{
+    public static class Hasher
+    {
+        public static string HashPassword(string password, string username)
+        {
+            byte[] salt = Encoding.UTF8.GetBytes(username);
+            byte[] passwordByte = Encoding.UTF8.GetBytes(password);
+            string result;
+            using (var sha = SHA256.Create())
+            {
+                int half = salt.Length / 2;
+                var firstHalf = salt.Take(half);
+                var secondHalf = salt.Skip(half);
+                var hashedBytes = sha.ComputeHash(firstHalf.Concat(passwordByte).Concat(secondHalf).ToArray());
+                result = BitConverter.ToString(hashedBytes).Replace("-", "").ToLowerInvariant();
+            }
+            return result;
+        }
+
+        public static bool IsPasswordValid(string username, string storedPassword, string givenPassword)
+        {
+            var givenPasswordHash = HashPassword(givenPassword, username);
+            return givenPasswordHash == storedPassword;
+        }
+    }
+}
---
+++ GsmRanking/Controllers/ValidationController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GsmRanking.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GsmRanking.Controllers
+{
+    public class ValidationController : Controller
+    {
+        private readonly IUserService _userService;
+
+        //public ValidationController(IUserService userService)
+        //{
+        //    _userService = userService;
+        //}
+
+        [HttpGet]
+        public async Task<JsonResult> VerifyUsername(string username)
+        {
+            //if (await _userService.AnyAsync(x => x.Username == username))
+            //{
+            //    return Json(data: $"Username {username} is already in use.");
+            //}
+            return Json(data: true);
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> VerifyEmail(string email)
+        {
+            //if (await _userService.AnyAsync(x => x.Email == email))
+            //{
+            //    return Json(data: $"Email {email} is already in use.");
+            //}
+            return Json(data: true);
+        }
+    }
+}
