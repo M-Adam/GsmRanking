be88208517a9835accf6e84bb83a8cabc2300077
Adam Mołczan <adam.molczan@outlook.com>
2017-04-09 16:05:13 +0200

    FA tag helper

---
+++ GsmRanking/TagHelpers/FontAwesomeTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GsmRanking.TagHelpers
+{
+    [HtmlTargetElement("fa", TagStructure = TagStructure.WithoutEndTag)]
+    public class FontAwesomeTagHelper : TagHelper
+    {
+        [HtmlAttributeName("class")]
+        public string Classes { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            ProcessAsync(context, output).Wait();
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+            output.Attributes.SetAttribute("aria-hidden", true);
+            output.Attributes.SetAttribute("class", $"fa {Classes}");
+        }
+    }
+}
